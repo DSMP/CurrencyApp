522e45e82b2c2f3d356be6ec94aaebd25e4083ed
damian <damiakuz@wp.pl>
2018-03-18 19:42:08 +0100

    główna strona zrobiona

--- CurrencyAppNative/ViewModels/MainMenuViewModel.cs
+++ CurrencyAppNative/ViewModels/MainMenuViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CurrencyAppShared.Models;
+﻿using CurrencyAppNative.UWPUtils;
+using CurrencyAppShared.Models;
 using CurrencyAppShared.Utils;
 using System;
 using System.Collections.Generic;
@@ -6,18 +7,23 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
 
 namespace CurrencyAppNative.ViewModels
 {
     class MainMenuViewModel : ViewModelBase
     {
         public ObservableCollection<Currency> Currencies { get; }
+        public ICommand ExitCommand { get; }
         public MainMenuViewModel()
         {
+            ExitCommand = new CommandHandler(() => Application.Current.Exit());
             Currencies = new ObservableCollection<Currency>();
             Currencies.Add(new Currency { CurrencyVal = 5.43m, Name = "qwe" });
             Currencies.Add(new Currency { CurrencyVal = 3.43m, Name = "wer" });
             Currencies.Add(new Currency { CurrencyVal = 2.43m, Name = "rty" });
         }
+
     }
 }
