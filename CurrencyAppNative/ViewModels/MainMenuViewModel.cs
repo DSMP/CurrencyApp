3fbc59f1c25e569b3a8993b09ef96b49dbc0f9c6
damian <damiakuz@wp.pl>
2018-03-18 19:17:36 +0100

    initial with MainMenu without exit button

---
+++ CurrencyAppNative/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CurrencyAppShared.Models;
+using CurrencyAppShared.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyAppNative.ViewModels
+{
+    class MainMenuViewModel : ViewModelBase
+    {
+        public ObservableCollection<Currency> Currencies { get; }
+        public MainMenuViewModel()
+        {
+            Currencies = new ObservableCollection<Currency>();
+            Currencies.Add(new Currency { CurrencyVal = 5.43m, Name = "qwe" });
+            Currencies.Add(new Currency { CurrencyVal = 3.43m, Name = "wer" });
+            Currencies.Add(new Currency { CurrencyVal = 2.43m, Name = "rty" });
+        }
+    }
+}
---
+++ CurrencyAppShared/Models/Currency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyAppShared.Models
+{
+    public class Currency
+    {
+        public string Name;
+        public Decimal CurrencyVal;
+    }
+}
