95c3fd3aebb5bbb29c83e5483633dd7b86b05f15
damian <damiakuz@wp.pl>
2018-03-26 10:34:53 +0200

    dodano namespace xaml toolkit do generowanie wykresu

--- CurrencyAppNative/ViewModels/CurrencyHistoryViewModel.cs
+++ CurrencyAppNative/ViewModels/CurrencyHistoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CurrencyAppShared.Models;
+﻿using CurrencyAppNative.UWPUtils;
+using CurrencyAppShared.Models;
 using CurrencyAppShared.Utils;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CurrencyAppNative.ViewModels
 {
@@ -15,20 +17,22 @@ namespace CurrencyAppNative.ViewModels
         public Currency SelectedCurrency { get { return _selectedCurrency; } set { SetProperty(ref _selectedCurrency, value); localsettings.Values["selected_currency"] = _xMLParser.ObjToXML<Currency>(value); } }
         string _header;
         public string Header { get { return _header == null ? "Historia kursu " + _selectedCurrency.Name : _header; } set { SetProperty(ref _header, value); } }
-        DateTime _dateTimeStart;
-        public DateTime DateTimeStart { get { return _dateTimeStart; } set { SetProperty(ref _dateTimeStart, value); } }
-        DateTime _dateTimeFinish;
-        public DateTime DateTimeFinish { get { return _dateTimeFinish; } set { SetProperty(ref _dateTimeFinish, value); } }
+        DateTimeOffset _dateTimeStart;
+        public DateTimeOffset DateTimeStart { get { return _dateTimeStart; } set { SetProperty(ref _dateTimeStart, value); } }
+        DateTimeOffset _dateTimeFinish;
+        public DateTimeOffset DateTimeFinish { get { return _dateTimeFinish; } set { SetProperty(ref _dateTimeFinish, value); } }
         double _progress;
         public double Progress { get { return _progress; } set { SetProperty(ref _progress, value); } }
         Windows.Storage.StorageFolder localFolder;
         Windows.Storage.ApplicationDataContainer localsettings;
         IXMLParser _xMLParser;
+        
 
         public CurrencyHistoryViewModel()
         {
             localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
             localsettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            
             _xMLParser = new XMLParser();
             localsettings.Values["page"] = 2;
             if (localsettings.Values["selected_currency"] == null)
