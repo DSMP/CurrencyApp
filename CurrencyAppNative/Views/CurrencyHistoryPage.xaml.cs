e621ea38dd39b655da586221a6d23bc3c96ea7c1
damian <damiakuz@wp.pl>
2018-03-27 18:08:23 +0200

    refactor zapisu do pliku grafu

--- CurrencyAppNative/Views/CurrencyHistoryPage.xaml.cs
+++ CurrencyAppNative/Views/CurrencyHistoryPage.xaml.cs
@@ -81,35 +81,18 @@ namespace CurrencyAppNative.Views
         {
             FileSavePicker savePicker = new FileSavePicker();
             savePicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-            //MemoryStream ms = await Composition.WriteableBitmapRenderExtensions.RenderToPngStream(Chart);
-            savePicker.FileTypeChoices.Add("Picture", new List<string>() { ".png" });
-            // Default file name if the user does not type one in or select a file to replace
+            savePicker.FileTypeChoices.Add("Picture", new List<string>() { ".jpg" });
             savePicker.SuggestedFileName = "New Diagram";
-            //Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
-            //if (file != null)
-            //{
-            //    Windows.Storage.CachedFileManager.DeferUpdates(file);
-
-            //    await Windows.Storage.FileIO.WriteTextAsync(file, file.Name);
-
-            //    Windows.Storage.Provider.FileUpdateStatus status =
-            //        await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
-            //}
-
-            //Rendu du composant Xaml, ici le graphique 'Syncfusion.Chart', sous forme d'image en mémoire.
             RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap();
             await renderTargetBitmap.RenderAsync(lineChart, (int)lineChart.ActualWidth, (int)lineChart.ActualHeight);
             var pixelBuffer = await renderTargetBitmap.GetPixelsAsync();
 
-            //var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
             var localFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
-            //var saveFile = await localFolder.CreateFileAsync("Chart.png", Windows.Storage.CreationCollisionOption.GenerateUniqueName);
             var saveFile = await savePicker.PickSaveFileAsync();
 
-            // Encodage de l'image en mémoire dans le fichier désigné sur le disque
             using (var fileStream = await saveFile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
             {
-                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, fileStream);
 
                 encoder.SetPixelData(
                     BitmapPixelFormat.Bgra8,
